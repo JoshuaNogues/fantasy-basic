99fe12ae13254a5396eaf779260f547571356661
JoshuaNogues <120759313+JoshuaNogues@users.noreply.github.com>
2025-08-22 16:17:51 -0400

    cors fix

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Enable CORS for front-end
 app.use(cors({
-  origin: ["http://localhost:5173", "https://myfantasyapp.netlify.app"], 
+  origin: ["http://localhost:5173", "https://veefivefantasy.netlify.app/"], 
 }));
 
 app.use(express.json());
